dd045e20ea2616bf5739800532e0e56a1155fb50
Kavya M <kavya.official5121@gmail.com>
2026-06-29 17:30:37 +0530

    Day 1: added backend and frontend structure

---
+++ backend/src/main/java/com/finova/backend/FinovaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finova.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinovaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinovaBackendApplication.class, args);
+	}
+
+}
